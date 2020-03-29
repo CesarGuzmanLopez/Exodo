5a72614c11a603ed9f70faa3ffd70eded2e198e4
CesarGuzmanLopez <88-8@live.com.mx>
2020-03-28 21:45:33 -0600

    Finalizacion Version 0

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,25 +5,23 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-	            <h1>
-                           	@can('manage-users')
-                        		 manejador usuario
-                           	@endcan
-                </h1> 
-              	<h2> 
-              	
-					@role("web-developer")
-              	 		web-developer	
-                   	@endrole
-               	</h2>    
-				 </div>
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif 
+	            <h1> 
+               	@can('manage-users')
+            		 manejador usuario
+               	@endcan
+                </h1>
+				<h2>
+				@role("web-developer")
+          	 		web-developer
+               	@endrole
+               	</h2>
+				</div>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="es">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,6 +21,7 @@
 </head>
 <body>
     <div id="app">
+    
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
